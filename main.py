193670c4eff271ed314a5396597d65eff2a451b9
Tatjana Spirkovic <tatjana.spirkovic@gmail.com>
2023-02-19 00:45:32 +0100

    Guess game

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world")
+import random as rand
+print("Unesite bilo koji broj od nula do sto")
+x = rand.randint(0, 100)
+
+while True:
+    u = int(input("Unesite broj: "))
+    if u > x:
+        print("Vas broj je veci od nasumicno izabranog broja")
+    elif u < x:
+        print("Vas broj je manji od nasumicno izabranog broja")
+    else:
+        print("Vas broj i nasumicno izabrani broj su jednaki")
+        break
+
+print("Bravo")
